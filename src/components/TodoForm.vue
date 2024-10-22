c79cea216cebb0cb86000538b383d9d1ec07da79
Abhinav Goel <153209537+AbhinavGoel9@users.noreply.github.com>
2024-10-22 14:29:15 +0530

    Create TodoForm.vue

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <input v-model="newTodo" placeholder="Add a new todo" required />
+    <button type="submit">Add</button>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  emits: ['add-todo'],
+  setup(props) {
+    const newTodo = ref('');
+
+    const submitForm = () => {
+      props['add-todo']({ id: Date.now(), text: newTodo.value, completed: false });
+      newTodo.value = '';
+    };
+
+    return { newTodo, submitForm };
+  },
+};
+</script>
