12758b1739e47358f2793bcb3b79735b6cc4d3ae
Abhinav Goel <153209537+AbhinavGoel9@users.noreply.github.com>
2024-10-22 14:29:55 +0530

    Create TodoItem.vue

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <li>
+    <span :class="{ completed: todo.completed }" @click="toggleComplete">{{ todo.text }}</span>
+    <button @click="removeTodo">Remove</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: Object,
+  },
+  emits: ['remove-todo', 'toggle-complete'],
+  setup(props) {
+    const removeTodo = () => {
+      props['remove-todo'](props.todo.id);
+    };
+
+    const toggleComplete = () => {
+      props['toggle-complete'](props.todo.id);
+    };
+
+    return { removeTodo, toggleComplete };
+  },
+};
+</script>
+
+<style scoped>
+.completed {
+  text-decoration: line-through;
+}
+</style>
